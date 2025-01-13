4201be65d853c2665fccb1f207fb2b6e54fc9511
GABRIELA SANTOS SOUZA SILVA <gabriela.sssilva3@senacsp.edu.br>
2025-01-13 19:58:17 -0300

    first commit

---
+++ logica_programacao/ex02/Program.cs
@@ -0,0 +1,5 @@
+﻿// Ex.02 - Escreva um program que pergunte o nome do usuário, armazene esse nome numa variável name e apresente uma mensagem de boas-vindas exibindo o nome do usuário
+Console.WriteLine("Informe o nome:");
+String name = Console.ReadLine();
+
+Console.WriteLine("Seja bem-vindo(a)" + name);
