5e03f3d80066d500fe6fe4b02a0bf88a2f821bd4
GABRIELA SANTOS SOUZA SILVA <gabriela.sssilva3@senacsp.edu.br>
2025-01-13 22:06:50 -0300

    ex04

---
+++ logica_programacao/ex04/Program.cs
@@ -0,0 +1,30 @@
+﻿// Tipos de dados
+
+string nome = "Gabriela";
+Console.WriteLine("O nome do usuário é:" +nome);
+
+int idade = 24;
+//Console.Writeline("A idade do usuário é: " + idade + "anos");
+Console.WriteLine($"A idade do usuário é {idade} anos");
+
+float carteira = 2.63F;
+Char Classificacao = 'A'; 
+Console.WriteLine($"O cliente é do tipo {Classificacao}");
+bool dinheiroNaConta = true;
+Console.WriteLine(dinheiroNaConta);
+
+double limiteCredito = 2000.0;
+Console.WriteLine( "Seu limite de crédito é: " + limiteCredito);
+
+String sobrenome = " Silva";
+
+String nomeCompleto = nome + sobrenome;
+Console.WriteLine(nomeCompleto);
+
+double valorConta = 53.73;
+Console.WriteLine($"O valor na conta é: {valorConta} ");
+
+double limiteTotal = valorConta + limiteCredito;
+Console.WriteLine($"O saldo + Limite é {limiteTotal}"); 
+
+
